43e4a399877add4e194a7b75ff76a83ff405bfb5
Hatimabdullah <hatimalqiamh@gmail.com>
2020-12-15 17:02:27 +0300

    park

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0302d6ab58f72ad656a21bb4065eaebf'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e6bb5b55-7bba-43dc-aa20-cd3abcde9178'
+};
